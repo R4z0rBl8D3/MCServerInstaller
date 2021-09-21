a2df84d4c50d3d7d318753bd553ce0e7ee128864
hewo <rthongthipaya@gmail.com>
2021-09-21 18:16:51 -0400

    Add project files.

---
+++ MCServerInstaller/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.IO.Compression;
+
+namespace MCServerInstaller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += onLoad;
+        }
+
+        private void onLoad(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
