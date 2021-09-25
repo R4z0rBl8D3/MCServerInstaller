270b47c68b9d051b73de892d9dee0b6a2ed5e96d
hewo <rthongthipaya@gmail.com>
2021-09-24 20:55:25 -0400

    Added loading function to edit window

--- MCServerInstaller/EditWindow.xaml.cs
+++ MCServerInstaller/EditWindow.xaml.cs
@@ -41,6 +41,155 @@ namespace MCServerInstaller
                     line = sr.ReadLine();
                 }
             }
+            foreach (string property in properties)
+            {
+                if (property.StartsWith("allow-flight"))
+                {
+                    if (property.Split('=')[1] == "true")
+                    {
+                        AllowFlightCheckbox.IsChecked = true;
+                    }
+                    else
+                    {
+                        AllowFlightCheckbox.IsChecked = false;
+                    }
+                }
+                if (property.StartsWith("allow-nether"))
+                {
+                    if (property.Split('=')[1] == "true")
+                    {
+                        AllowNetherCheckbox.IsChecked = true;
+                    }
+                    else
+                    {
+                        AllowNetherCheckbox.IsChecked = false;
+                    }
+                }
+                if (property.StartsWith("force-gamemode"))
+                {
+                    if (property.Split('=')[1] == "true")
+                    {
+                        ForceGamemodeCheckbox.IsChecked = true;
+                    }
+                    else
+                    {
+                        ForceGamemodeCheckbox.IsChecked = false;
+                    }
+                }
+                if (property.StartsWith("hardcore"))
+                {
+                    if (property.Split('=')[1] == "true")
+                    {
+                        HardcoreCheckbox.IsChecked = true;
+                    }
+                    else
+                    {
+                        HardcoreCheckbox.IsChecked = false;
+                    }
+                }
+                if (property.StartsWith("online-mode"))
+                {
+                    if (property.Split('=')[1] == "true")
+                    {
+                        OnlineModeCheckbox.IsChecked = true;
+                    }
+                    else
+                    {
+                        OnlineModeCheckbox.IsChecked = false;
+                    }
+                }
+                if (property.StartsWith("pvp"))
+                {
+                    if (property.Split('=')[1] == "true")
+                    {
+                        PVPCheckbox.IsChecked = true;
+                    }
+                    else
+                    {
+                        PVPCheckbox.IsChecked = false;
+                    }
+                }
+                if (property.StartsWith("spawn-animals"))
+                {
+                    if (property.Split('=')[1] == "true")
+                    {
+                        SpawnAnimalsCheckbox.IsChecked = true;
+                    }
+                    else
+                    {
+                        SpawnAnimalsCheckbox.IsChecked = false;
+                    }
+                }
+                if (property.StartsWith("spawn-monsters"))
+                {
+                    if (property.Split('=')[1] == "true")
+                    {
+                        SpawnMonstersCheckbox.IsChecked = true;
+                    }
+                    else
+                    {
+                        SpawnMonstersCheckbox.IsChecked = false;
+                    }
+                }
+                if (property.StartsWith("spawn-npcs"))
+                {
+                    if (property.Split('=')[1] == "true")
+                    {
+                        SpawnNPCsCheckbox.IsChecked = true;
+                    }
+                    else
+                    {
+                        SpawnNPCsCheckbox.IsChecked = false;
+                    }
+                }
+                if (property.StartsWith("enable-command-block"))
+                {
+                    if (property.Split('=')[1] == "true")
+                    {
+                        EnableCommandBlocksCheckbox.IsChecked = true;
+                    }
+                    else
+                    {
+                        EnableCommandBlocksCheckbox.IsChecked = false;
+                    }
+                }
+                if (property.StartsWith("difficulty"))
+                {
+                    DifficultyBox.Text = property.Split('=')[1];
+                }
+                if (property.StartsWith("server-ip"))
+                {
+                    ServerIPBox.Text = property.Split('=')[1];
+                }
+                if (property.StartsWith("server-port"))
+                {
+                    ServerPortBox.Text = property.Split('=')[1];
+                }
+                if (property.StartsWith("motd"))
+                {
+                    MOTDBox.Text = property.Split('=')[1];
+                }
+            }
+            using (StreamReader sr = new StreamReader(MainWindow.editPath + "\\StartServer.bat"))
+            {
+                sr.ReadLine();
+                string line = sr.ReadLine();
+                MemoryBox.Text = line.Split(' ')[1].Split('x')[1];
+            }
+            using (StreamReader sr = new StreamReader(MainWindow.editPath + "\\StartServer.bat"))
+            {
+                string line = sr.ReadLine();
+                bool gui = true;
+                while (line != null)
+                {
+                    if (line.Contains("nogui"))
+                    {
+                        gui = false;
+                    }
+                    line = sr.ReadLine();
+                }
+                GuiCheckbox.IsChecked = gui;
+            }
             if (Directory.Exists(MainWindow.editPath + "\\mods"))
             {
                 StatusTxt.Content = "Loading mods...";
@@ -51,12 +200,35 @@ namespace MCServerInstaller
                     ModsListBox.Items.Add(mod.Split('\\')[mod.Split('\\').Length - 1]);
                 }
             }
+            if (Directory.Exists(MainWindow.editPath + "\\scripts"))
+            {
+                StatusTxt.Content = "Loading scripts...";
+                ScriptsListBox.Items.Clear();
+                string[] scripts = Directory.GetFiles(MainWindow.editPath + "\\scripts");
+                foreach (string script in scripts)
+                {
+                    ScriptsListBox.Items.Add(script.Split('\\')[script.Split('\\').Length - 1]);
+                }
+            }
+            if (Directory.Exists(MainWindow.editPath + "\\structures"))
+            {
+                StatusTxt.Content = "Loading structures...";
+                StructuresListBox.Items.Clear();
+                string[] structures = Directory.GetFiles(MainWindow.editPath + "\\structures");
+                foreach (string structure in structures)
+                {
+                    StructuresListBox.Items.Add(structure.Split('\\')[structure.Split('\\').Length - 1]);
+                }
+            }
         }
 
-        private void onLoad(object sender, RoutedEventArgs e)
+        private async void onLoad(object sender, RoutedEventArgs e)
         {
-            load();
-            
+            while (true)
+            {
+                load();
+                await Task.Delay(1000);
+            }
         }
 
         private void save()
